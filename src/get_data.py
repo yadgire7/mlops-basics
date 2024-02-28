6c0aa44f756b879abab6f40cb4dc0539d503727a
“saurabh145yadgire@gmail.com” <saurabh145yadgire@gmail.com>
2024-02-28 12:49:09 -0800

    added get_data.py in src directory

---
+++ src/get_data.py
@@ -0,0 +1,25 @@
+# import required libraries
+import os
+import pandas as pd
+import yaml
+import argparse
+
+def read_params(config_path):
+    with open("params.yaml", "r") as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+# method to get data
+def get_data(config_path):
+    config = read_params(config_path)
+    data_path = config['data_source']['s3_source']
+    df = pd.read_csv(data_path, sep=";", encoding='utf-8')
+    return df
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default= "params.yaml")
+    parsed_args = args.parse_args()
+    data = get_data(config_path=parsed_args.config)
+
+
